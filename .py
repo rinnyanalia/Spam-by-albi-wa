b2884fa8e99c42204e0dd8d487c08def372e793a
rinnyanalia <168186545+rinnyanalia@users.noreply.github.com>
2024-04-30 14:44:40 +0700

    Spam .py
    Script ini sewaktu waktu bisa mokad

---
+++ .py
@@ -0,0 +1,19 @@
+print("[+]==================[+]")
+print("[+]= author by albi =[+]")
+print("[+]==================[+]")
+print(" ")
+print("script ini sewaktu waktu bisa mokad")
+print("batas jumlah spam 10")
+a = int(input("masukan nomor target : "))
+b = int(input("masukan jumlah target : "))
+
+for i in range(b):
+	if(b == 10):
+		print("berhasil✅")
+	else:
+		print("jumlah yang kamu masukan lebih dari 10")
+	
+print("terima kasih telah menggunakan layanan kami")
+
+print("nomor target =",a)
+print("jumlah spam =",b)
